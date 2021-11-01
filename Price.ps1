b237c8c987cafc4e395393f02b24e5eb5e837c4b
Gurjot Singh <38872085+sing0978@users.noreply.github.com>
2021-11-01 17:34:26 -0400

    Update Price.ps1

--- Price.ps1
+++ Price.ps1
@@ -1,8 +1,8 @@
 ﻿
 $WebResponse = Invoke-WebRequest "https://learning.oreilly.com/signup/"
 $price = $WebResponse.AllElements | where data-cy -eq "plan-price"  | select innertext | Select -Last 1 -ExpandProperty innertext 
-$username = 'dummyqdummy1@gmail.com'  
-$password = "#Guru@123" 
+$username = '<email address>'  
+$password = "<password>" 
 $sec = ConvertTo-SecureString $password -AsPlainText -Force 
 $cred = New-Object System.Management.Automation.PSCredential ($username, $sec) 
-Send-MailMessage -from $username -to deepsimran7377@gmail.com -subject "O'Reilly Price (Monthly): $price" -Body "$price" -SmtpServer smtp.gmail.com -Credential $cred -UseSsl
+Send-MailMessage -from $username -to <email> -subject "O'Reilly Price (Monthly): $price" -Body "$price" -SmtpServer smtp.gmail.com -Credential $cred -UseSsl
